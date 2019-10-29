0ae5577bd653b11ba6fa4597f0638b0ec2680a08
Student <student@makerss-mbp.home>
2019-10-29 22:20:08 +0000

    Thermostat can increase or decrease temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,4 +8,15 @@ describe('Thermostat', function () {
   it('starts at 20 degrees', function () {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  it('increase the temperature', function () {
+    thermostat.up()
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it('decrease the temperature', function () {
+    thermostat.down()
+    expect(thermostat.temperature).toEqual(19)
+  })
+  
 })
