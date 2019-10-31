3958c89413b0a029aff3509146bde5e5ae722297
Student <student@Makerss-MacBook-Pro.local>
2019-10-31 13:45:13 +0000

    Weather api

--- jquery/main.js
+++ jquery/main.js
@@ -35,13 +35,29 @@ $(document).ready(function () {
   });
 
   function updateTemperature() {
-    $('#temperature').text(thermostat.temperature);
-    $('#temperature').attr('class', thermostat.energyUsage());
+    console.log(thermostat.energyUsage())
+    temperature.textContent = thermostat.temperature
+    temperature.setAttribute('class', thermostat.energyUsage());
   }
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=1749f23fb2b07d86d0670b648fe72619', function (data) {
-    console.log(data);
-  })
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=1749f23fb2b07d86d0670b648fe72619', true);
+
+  request.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      var data = JSON.parse(this.response);
+    } else {
+      // We reached our target server, but it returned an error
+
+    }
+  };
+
+  request.onerror = function () {
+    // There was a connection error of some sort
+  };
+
+  request.send();
 
   $('#current-city').change(function () {
     var city = $('#current-city').val();
