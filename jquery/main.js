1f05afdd560790b59bd6c535dd157eb79cca68b5
Student <student@Makerss-MacBook-Pro.local>
2019-10-31 12:48:17 +0000

    Change jquery to ES6

--- jquery/main.js
+++ jquery/main.js
@@ -1,39 +1,42 @@
 
 $(document).ready(function () {
-  
+
   var thermostat = new Thermostat();
   updateTemperature();
 
-  $('#temp-up').click(function () {
+  tempUp.addEventListener("click", function () {
     thermostat.up();
     updateTemperature();
   });
 
-  $('#temp-down').click(function () {
+  tempDown.addEventListener('click', function () {
     thermostat.down();
     updateTemperature();
   });
 
-  $('#PSM-on').click(function () {
+  powersavingOn.addEventListener('click', function () {
+    console.log('click')
     thermostat.powerSavingModeOn();
     $('#powerSavingModeOn').text('on')
     updateTemperature()
   })
 
-  $('#PSM-off').click(function () {
+  powersavingOff.addEventListener('click', function () {
+    console.log('click')
     thermostat.powerSavingModeOff();
     $('#powerSavingModeOff').text('off')
     updateTemperature();
   })
 
-  $('#temp-reset').click(function () {
+  tempReset.addEventListener('click', function () {
+    console.log('click')
     thermostat.resetTemperature();
     updateTemperature()
-  })
+  });
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
-    $('#temperature').attr('class', thermostat.energyUsage()); 
+    $('#temperature').attr('class', thermostat.energyUsage());
   }
 
   $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=1749f23fb2b07d86d0670b648fe72619', function (data) {
@@ -45,6 +48,6 @@ $(document).ready(function () {
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function (data) {
       $('#current-temperature').text(data.main.temp)
     })
-    
+
   })
 });
